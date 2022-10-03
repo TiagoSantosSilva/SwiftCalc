024309495e931ece0ab4f3bfe2e996942c6de97c
Tiago Silva <tiagossilva_95@hotmail.com>
2022-10-03 16:31:33 +0100

    Initial Commit

---
+++ SwiftCalc/SwiftCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCalcApp.swift
+//  SwiftCalc
+//
+//  Created by Tiago Silva on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
